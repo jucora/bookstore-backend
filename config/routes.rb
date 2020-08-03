163d32d5174dbdc65b2ea8d5ccd49adb8bbf74ac
jucora <artjulius@hotmail.com>
2020-08-03 10:29:30 -0500

    Add Book controller with main verbs

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/show'
+  get 'books/create'
+  get 'books/destroy'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
