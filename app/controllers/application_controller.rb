e3f45f96eebc16d03972e90a9d8ae9ff13bbac6a
jucora <artjulius@hotmail.com>
2020-08-04 12:29:16 -0500

    Add headers in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+    before_action :allow_cross_domain
+  def allow_cross_domain
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Request-Method'] = 'POST, OPTIONS'
+  end
 end
