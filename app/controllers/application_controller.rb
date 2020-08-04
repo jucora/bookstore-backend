f237931c8de24e7057fbcc5c57d61b20eae8a428
jucora <artjulius@hotmail.com>
2020-08-04 12:38:29 -0500

    Add config.middleware in application.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,3 @@
 class ApplicationController < ActionController::API
-    before_action :allow_cross_domain
-  def allow_cross_domain
-      headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Request-Method'] = 'POST, OPTIONS'
-  end
+    
 end
